94595715d6d6177d83ada09ccb73de47428b12f2
Nick Giegerich <nickgiegerich@u.boisestate.edu>
2022-01-24 15:41:40 -0700

    nav updates

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,10 +1,11 @@
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
 import Link from 'next/link';
 
 const LINKS = ["Home", "Services", "About", "Team", "Contact"];
 const logoImg = "/images/logos/fwm.png";
 
 function Nav() {
+    const [showMenu, setShowMenu] = useState(false)
 
     return (
         <header className="border-b border-gray-400 relative sm:sticky top-0 z-20 bg-white shadow-lg">
@@ -28,21 +29,49 @@ function Nav() {
                     </Link>
                 </div>
 
-                    {LINKS.map((lnk, idx) => (
-                        <div key={idx} className="max-w-xs w-1/2 text-center sm:text-right">
-                            <Link href={`/${lnk}`}>
-                                <h1>
-                                    <a className=" cursor-pointer">
-                                        <span className="text-xl font-primary font-bold tracking-tight pt-1 hover:underline">
-                                            {lnk}
-                                        </span>
-                                    </a>
-                                </h1>
-                            </Link>
-                        </div>
+                {LINKS.map((lnk, idx) => (
+                    <div key={idx} className="hidden md:flex max-w-xs w-1/2 text-center sm:text-right">
+                        <Link href={`/${lnk}`}>
+                            <h1>
+                                <a className="cursor-pointer">
+                                    <span className="text-xl font-primary font-bold tracking-tight pt-1 hover:underline">
+                                        {lnk}
+                                    </span>
+                                </a>
+                            </h1>
+                        </Link>
+                    </div>
 
-                    ))}
+                ))}
+                {/* <!-- Mobile menu button --> */}
+                <div className="md:hidden flex items-center">
+                    <button className="outline-none" onClick={() => setShowMenu(!showMenu)}>
+                        <svg className="w-6 h-6 text-gray-500 hover:text-blue-300"
+                            xlinkShow="!showMenu"
+                            fill="none"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                        >
+                            <path d="M4 6h16M4 12h16M4 18h16"></path>
+
+                        </svg>
+                    </button>
+                </div>
+                {/* <!-- mobile menu --> */}
             </div>
+            {showMenu && (
+                <div className="md:hidden">
+                    <ul>
+                        <li><a className="block text-sm px-2 py-4 text-black bg-blue-50 font-semibold hover:bg-orange-300 transition duration-300">Home</a></li>
+                        <li><a className="block text-sm px-2 py-4 text-black bg-blue-50 font-semibold hover:bg-orange-300 transition duration-300">Home</a></li>
+                        <li><a className="block text-sm px-2 py-4 text-black bg-blue-50 font-semibold hover:bg-orange-300 transition duration-300">Home</a></li>
+                        <li><a className="block text-sm px-2 py-4 text-black bg-blue-50 font-semibold hover:bg-orange-300 transition duration-300">Home</a></li>
+                    </ul>
+                </div>
+            )}
         </header>
     )
 }
