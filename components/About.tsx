568ec5c6efa32ac70ca9776ffa4bbff90b891420
Nick Giegerich <nickgiegerich@u.boisestate.edu>
2022-01-27 09:01:15 -0700

    fixes for vercel

--- components/About.tsx
+++ components/About.tsx
@@ -10,8 +10,8 @@ function About() {
         <div className="flex flex-col justify-evenly items-center">
             <span className="text-4xl font-semibold uppercase">who we are</span>
             <span className="text-center text-2xl font-sans text-opacity-75 text-gray-600 max-w-md md:max-w-6xl py-10 leading-relaxed">
-                We've been helping brands develop and define their voice since 2016. The times may have changed, but our
-                love for creativity and problem-solving hasn't. We're driven by technology and innovation to ensure we
+                We&apos;ve been helping brands develop and define their voice since 2016. The times may have changed, but our
+                love for creativity and problem-solving hasn&apos;t. We&apos;re driven by technology and innovation to ensure we
                 fulfill our key mission of helping our clients find their successful future. Get in touch so that we can
                 kickstart your branding today.
             </span>
