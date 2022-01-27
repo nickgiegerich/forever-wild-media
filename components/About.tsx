46702165313ebf25ccd6f583b45d307ff4d13263
Nick Giegerich <nickgiegerich@u.boisestate.edu>
2022-01-27 13:04:24 -0700

    nav clean up

--- components/About.tsx
+++ components/About.tsx
@@ -9,7 +9,7 @@ function About() {
     return (
         <div className="flex flex-col justify-evenly items-center">
             <span className="text-4xl font-semibold uppercase">who we are</span>
-            <span className="text-center text-2xl font-sans text-opacity-75 text-gray-600 max-w-md md:max-w-6xl py-10 leading-relaxed">
+            <span className="text-center text-2xl font-sans text-opacity-75 text-gray-600 max-w-xs md:max-w-6xl py-10 leading-relaxed">
                 We&apos;ve been helping brands develop and define their voice since 2016. The times may have changed, but our
                 love for creativity and problem-solving hasn&apos;t. We&apos;re driven by technology and innovation to ensure we
                 fulfill our key mission of helping our clients find their successful future. Get in touch so that we can
--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,21 +1,44 @@
-import { ReactNode, useEffect, useState } from "react";
+import { createRef, useEffect, useRef, useState } from "react";
 import Link from 'next/link';
 
 const LINKS = ["Home", "Services", "About", "Team", "Contact"];
 const logoImg = "/images/logos/fwm.png";
 
 function Nav() {
+    const navRef = useRef(null)
     const [showMenu, setShowMenu] = useState(false)
     const [offset, setOffset] = useState(0);
+
+    const handleOutsideClick = (e: MouseEvent) => {
+        if (!this.node.contains(e.target)) handleClick();
+    }
+
+    const handleClick = () => {
+        if (!showMenu) {
+            document.addEventListener("click", handleOutsideClick, false);
+        } else {
+            document.removeEventListener('click', handleOutsideClick, false);
+        }
+
+        setShowMenu(!showMenu)
+    }
 
     useEffect(() => {
         window.onscroll = () => {
             setOffset(window.scrollY);
         };
+        // document.onclick = (e: MouseEvent) => {
+        //     console.log(e.target)
+        //     // @ts-ignore
+        //     if (navRef.current && !navRef.current.contains(e.target)) {
+        //         setShowMenu(false)
+        //         console.log('click out side')
+        //     }
+        // }
     }, [])
 
     return (
-        <header className={`top-0 bg-transparent text-slate-100 transition-all duration-300 fixed w-full z-20 ${offset > 85 ? "scale-1 bg-white text-black shadow-2xl py-3" : "py-5"} `}>
+        <header ref={node => {node = node}} className={`top-0 text-slate-100 transition-all duration-300 fixed w-full z-50 ${offset > 85 ? "scale-1 bg-white text-black shadow-2xl py-3" : "py-5"} ${showMenu ? "bg-white" : "bg-transparent"} `}>
             <div className="flex items-center justify-evenly mx-auto max-w-7xl px-4">
                 <div className="max-w-lg w-1/2">
                     <Link href={"/"} passHref>
@@ -53,7 +76,7 @@ function Nav() {
                 {/* <!-- Mobile menu button --> */}
                 <div className="md:hidden flex items-center">
                     <button className="outline-none" onClick={() => setShowMenu(!showMenu)}>
-                        <svg className={`w-10 h-10 hover:text-black ${offset > 85 ? "text-black" : "text-slate-100 "}`}
+                        <svg className={`w-10 h-10 hover:text-black ${offset > 85 ? "text-black" : "text-black "}`}
                             xlinkShow="!showMenu"
                             fill="none"
                             strokeLinecap="round"
@@ -70,13 +93,13 @@ function Nav() {
                 {/* <!-- mobile menu --> */}
             </div>
             {showMenu && (
-                <div className="md:hidden transition-all transform scale-y-100 bg-black">
+                <div className="md:hidden transition-all duration-300 transform mt-4 bg-black">
                     <ul>
-                        <li><a className="block text-sm px-2 py-4 text-black bg-blue-50 font-semibold hover:bg-orange-300 transition duration-300">Home</a></li>
-                        <li><a className="block text-sm px-2 py-4 text-black bg-blue-50 font-semibold hover:bg-orange-300 transition duration-300">Services</a></li>
-                        <li><a className="block text-sm px-2 py-4 text-black bg-blue-50 font-semibold hover:bg-orange-300 transition duration-300">About</a></li>
-                        <li><a className="block text-sm px-2 py-4 text-black bg-blue-50 font-semibold hover:bg-orange-300 transition duration-300">Team</a></li>
-                        <li><a className="block text-sm px-2 py-4 text-black bg-blue-50 font-semibold hover:bg-orange-300 transition duration-300">Contact</a></li>
+                        <li><a className="block text-sm px-2 py-4 text-black bg-white font-semibold hover:bg-orange-300 transition duration-300">Home</a></li>
+                        <li><a className="block text-sm px-2 py-4 text-black bg-white font-semibold hover:bg-orange-300 transition duration-300">Services</a></li>
+                        <li><a className="block text-sm px-2 py-4 text-black bg-white font-semibold hover:bg-orange-300 transition duration-300">About</a></li>
+                        <li><a className="block text-sm px-2 py-4 text-black bg-white font-semibold hover:bg-orange-300 transition duration-300">Team</a></li>
+                        <li><a className="block text-sm px-2 py-4 text-black bg-white font-semibold hover:bg-orange-300 transition duration-300">Contact</a></li>
                     </ul>
                 </div>
             )}
@@ -85,3 +108,4 @@ function Nav() {
 }
 
 export default Nav;
+
